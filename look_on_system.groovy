19f9de35afdb77e505ce2fa82961486a1cf2e9f8
chetan <chetansomkuwar45@gmail.com>
2022-07-14 11:58:46 +0530

    --- Auto Git Commit ---

---
+++ look_on_system.groovy
@@ -0,0 +1,10 @@
+job('jarvis2') {
+    scm {
+        git {
+            remote {
+                name('origin')
+                url('https://github.com/rupamdhote/drool.git')
+            }            
+        }
+    }
+}
